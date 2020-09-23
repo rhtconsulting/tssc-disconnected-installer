2376bcb43c2879d8ee49f07b5f1dba1dcbe34746
Dillon Winchell <dwinchel@redhat.com>
2020-09-23 16:14:42 -0400

    Add argument to upload-images.sh

--- upload-images.sh
+++ upload-images.sh
@@ -1,3 +1,4 @@
+REGISTRY=$1
 skopeo copy oci://${INSTALLER_DIR}/tssc-ci-agent-jenkins:v0.10 docker://$REGISTRY/tssc/tssc-ci-agent-jenkins:v0.10
 skopeo copy oci://${INSTALLER_DIR}/tssc-tool-maven:v0.10 docker://$REGISTRY/tssc/tssc-tool-maven:v0.10
 skopeo copy oci://${INSTALLER_DIR}/tssc-tool-buildah:v0.10 docker://$REGISTRY/tssc/tssc-tool-buildah:v0.10
