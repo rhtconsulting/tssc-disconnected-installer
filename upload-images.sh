cd3ed9af13a518fc028c442cf71e9cff2aba4a29
Dillon Winchell <dwinchel@redhat.com>
2020-09-24 12:09:10 -0400

    Replace INSTALLER_DIR with PACKAGE_DIR in upload-images.sh

--- upload-images.sh
+++ upload-images.sh
@@ -1,7 +1,7 @@
 REGISTRY=$1
-skopeo copy oci://${INSTALLER_DIR}/tssc-ci-agent-jenkins:v0.10 docker://$REGISTRY/tssc/tssc-ci-agent-jenkins:v0.10
-skopeo copy oci://${INSTALLER_DIR}/tssc-tool-maven:v0.10 docker://$REGISTRY/tssc/tssc-tool-maven:v0.10
-skopeo copy oci://${INSTALLER_DIR}/tssc-tool-buildah:v0.10 docker://$REGISTRY/tssc/tssc-tool-buildah:v0.10
-skopeo copy oci://${INSTALLER_DIR}/tssc-tool-argocd:v0.10 docker://$REGISTRY/tssc/tssc-tool-argocd:v0.10
-skopeo copy oci://${INSTALLER_DIR}/tssc-tool-skopeo:v0.10 docker://$REGISTRY/tssc/tssc-tool-skopeo:v0.10
-skopeo copy oci://${INSTALLER_DIR}/tssc-tool-sonar:v0.10 docker://$REGISTRY/tssc/tssc-tool-sonar:v0.10
+skopeo copy oci://${PACKAGE_DIR}/tssc-ci-agent-jenkins:v0.10 docker://$REGISTRY/tssc/tssc-ci-agent-jenkins:v0.10
+skopeo copy oci://${PACKAGE_DIR}/tssc-tool-maven:v0.10 docker://$REGISTRY/tssc/tssc-tool-maven:v0.10
+skopeo copy oci://${PACKAGE_DIR}/tssc-tool-buildah:v0.10 docker://$REGISTRY/tssc/tssc-tool-buildah:v0.10
+skopeo copy oci://${PACKAGE_DIR}/tssc-tool-argocd:v0.10 docker://$REGISTRY/tssc/tssc-tool-argocd:v0.10
+skopeo copy oci://${PACKAGE_DIR}/tssc-tool-skopeo:v0.10 docker://$REGISTRY/tssc/tssc-tool-skopeo:v0.10
+skopeo copy oci://${PACKAGE_DIR}/tssc-tool-sonar:v0.10 docker://$REGISTRY/tssc/tssc-tool-sonar:v0.10
