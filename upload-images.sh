a76072b87b604268d6c53875efb08b0a1713d308
Dillon Winchell <dwinchel@redhat.com>
2020-09-24 18:10:35 -0400

    Fixed tag in upload-images.sh

--- upload-images.sh
+++ upload-images.sh
@@ -1,8 +1,9 @@
 REGISTRY=$1
 IMAGE_DIR=${PACKAGE_DIR}/container-images
-skopeo copy oci://${IMAGE_DIR}/tssc-ci-agent-jenkins:v0.10 docker://$REGISTRY/tssc/tssc-ci-agent-jenkins:v0.10
-skopeo copy oci://${IMAGE_DIR}/tssc-tool-maven:v0.10 docker://$REGISTRY/tssc/tssc-tool-maven:v0.10
-skopeo copy oci://${IMAGE_DIR}/tssc-tool-buildah:v0.10 docker://$REGISTRY/tssc/tssc-tool-buildah:v0.10
-skopeo copy oci://${IMAGE_DIR}/tssc-tool-argocd:v0.10 docker://$REGISTRY/tssc/tssc-tool-argocd:v0.10
-skopeo copy oci://${IMAGE_DIR}/tssc-tool-skopeo:v0.10 docker://$REGISTRY/tssc/tssc-tool-skopeo:v0.10
-skopeo copy oci://${IMAGE_DIR}/tssc-tool-sonar:v0.10 docker://$REGISTRY/tssc/tssc-tool-sonar:v0.10
+TAG=v0.10.0
+skopeo copy oci://${IMAGE_DIR}/tssc-ci-agent-jenkins:${TAG} docker://$REGISTRY/tssc/tssc-ci-agent-jenkins:${TAG}
+skopeo copy oci://${IMAGE_DIR}/tssc-tool-maven:${TAG} docker://$REGISTRY/tssc/tssc-tool-maven:${TAG}
+skopeo copy oci://${IMAGE_DIR}/tssc-tool-buildah:${TAG} docker://$REGISTRY/tssc/tssc-tool-buildah:${TAG}
+skopeo copy oci://${IMAGE_DIR}/tssc-tool-argocd:${TAG} docker://$REGISTRY/tssc/tssc-tool-argocd:${TAG}
+skopeo copy oci://${IMAGE_DIR}/tssc-tool-skopeo:${TAG} docker://$REGISTRY/tssc/tssc-tool-skopeo:${TAG}
+skopeo copy oci://${IMAGE_DIR}/tssc-tool-sonar:${TAG} docker://$REGISTRY/tssc/tssc-tool-sonar:${TAG}
